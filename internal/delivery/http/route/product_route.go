7d4aef7ed487f9650dff9126e3bf486ec60432a1
Hackim18 <khakim668@gmail.com>
2025-08-12 12:41:48 +0700

    fix: fixing product route remove auth middleware

--- internal/delivery/http/route/product_route.go
+++ internal/delivery/http/route/product_route.go
@@ -10,10 +10,10 @@ import (
 func (c *RouteConfig) RegisterProductRoutes(rg *gin.RouterGroup, minioClient *minio.Client) {
 	product := rg.Group("/products")
 
-	product.GET("/", c.AuthMiddleware, c.ProductController.GetAllProducts)
-	product.GET("/:productID", c.AuthMiddleware, c.ProductController.GetProductByID)
-	product.GET("/search", c.AuthMiddleware, c.ProductController.SearchProducts)
-	product.POST("/", c.AuthMiddleware, c.ProductController.CreateProduct)
+	product.GET("/", c.ProductController.GetAllProducts)
+	product.GET("/:productID", c.ProductController.GetProductByID)
+	product.GET("/search", c.ProductController.SearchProducts)
+	product.POST("/", c.ProductController.CreateProduct)
 	product.PUT("/:productID", c.AuthMiddleware, c.ProductController.UpdateProduct)
 	product.POST("/:productID/images", c.AuthMiddleware, middleware.MultipleFileUpload(minioClient, middleware.UploadOptions{
 		FieldName:     "images",
@@ -22,8 +22,8 @@ func (c *RouteConfig) RegisterProductRoutes(rg *gin.RouterGroup, minioClient *mi
 		BucketName:    c.Viper.GetString("MINIO_BUCKET_PRODUCT"),
 		AllowedTypes:  []string{"image/jpeg", "image/png", "image/gif"},
 	}), c.ProductController.UploadProductImages)
-	product.GET("/image/:imageID/url", c.AuthMiddleware, c.ProductController.GetProductImageURL)
-	product.GET("/image/:imageID/preview", c.AuthMiddleware, c.ProductController.GetObjectImage)
+	product.GET("/image/:imageID/url", c.ProductController.GetProductImageURL)
+	product.GET("/image/:imageID/preview", c.ProductController.GetObjectImage)
 	product.DELETE("/:productID", c.AuthMiddleware, c.ProductController.DeleteProduct)
 	product.DELETE("/image/:imageID", c.AuthMiddleware, c.ProductController.DeleteProductImage)
 }
